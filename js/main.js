34b7e5cc22988fa71e0e114d5c0e14341badf26a
shriyaaa <shriyanarang03@gmail.com>
2020-07-01 02:12:37 +0400

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,213 @@
+(function ($) {
+ "use strict";
+
+	/*----------------------------
+	 jQuery MeanMenu
+	------------------------------ */
+	jQuery('nav#dropdown').meanmenu();	
+	/*----------------------------
+	 jQuery myTab
+	------------------------------ */
+	$('#myTab a').on('click', function (e) {
+		  e.preventDefault()
+		  $(this).tab('show')
+		});
+		$('#myTab3 a').on('click', function (e) {
+		  e.preventDefault()
+		  $(this).tab('show')
+		});
+		$('#myTab4 a').on('click', function (e) {
+		  e.preventDefault()
+		  $(this).tab('show')
+		});
+		$('#myTabedu1 a').on('click', function (e) {
+		  e.preventDefault()
+		  $(this).tab('show')
+		});
+
+	  $('#single-product-tab a').on('click', function (e) {
+		  e.preventDefault()
+		  $(this).tab('show')
+		});
+	
+	$('[data-toggle="tooltip"]').tooltip(); 
+	
+	$('#sidebarCollapse').on('click', function () {
+		 $('#sidebar').toggleClass('active');
+	 });
+	// Collapse ibox function
+	$('#sidebar ul li').on('click', function () {
+		var button = $(this).find('i.fa.indicator-mn');
+		button.toggleClass('fa-plus').toggleClass('fa-minus');
+		
+	});
+	/*-----------------------------
+		Menu Stick
+	---------------------------------*/
+	$(".sicker-menu").sticky({topSpacing:0});
+		
+	$('#sidebarCollapse').on('click', function () {
+		$("body").toggleClass("mini-navbar");
+		SmoothlyMenu();
+	});
+	$(document).on('click', '.header-right-menu .dropdown-menu', function (e) {
+		  e.stopPropagation();
+	});
+	/*----------------------------
+	 wow js active
+	------------------------------ */
+	 new WOW().init();
+	/*----------------------------
+	 owl active
+	------------------------------ */  
+	$("#owl-demo").owlCarousel({
+      autoPlay: false, 
+	  slideSpeed:2000,
+	  pagination:false,
+	  navigation:true,	  
+      items : 4,
+	  /* transitionStyle : "fade", */    /* [This code for animation ] */
+	  navigationText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+      itemsDesktop : [1199,4],
+	  itemsDesktopSmall : [980,3],
+	  itemsTablet: [768,2],
+	  itemsMobile : [479,1],
+	});
+	/*----------------------------
+	 price-slider active
+	------------------------------ */  
+	  $( "#slider-range" ).slider({
+	   range: true,
+	   min: 40,
+	   max: 600,
+	   values: [ 60, 570 ],
+	   slide: function( event, ui ) {
+		$( "#amount" ).val( "£" + ui.values[ 0 ] + " - £" + ui.values[ 1 ] );
+	   }
+	  });
+	  $( "#amount" ).val( "£" + $( "#slider-range" ).slider( "values", 0 ) +
+	   " - £" + $( "#slider-range" ).slider( "values", 1 ) );
+	/*--------------------------
+	 scrollUp
+	---------------------------- */	
+	$.scrollUp({
+        scrollText: '<i class="fa fa-angle-up"></i>',
+        easingType: 'linear',
+        scrollSpeed: 900,
+        animation: 'fade'
+    }); 	   
+	
+	
+	
+	
+	
+	
+	
+	$(document).ready(function () {
+    var itemsMainDiv = ('.MultiCarousel');
+    var itemsDiv = ('.MultiCarousel-inner');
+    var itemWidth = "";
+
+    $('.leftLst, .rightLst').click(function () {
+        var condition = $(this).hasClass("leftLst");
+        if (condition)
+            click(0, this);
+        else
+            click(1, this)
+    });
+
+    ResCarouselSize();
+
+
+
+
+    $(window).resize(function () {
+        ResCarouselSize();
+    });
+
+    //this function define the size of the items
+    function ResCarouselSize() {
+        var incno = 0;
+        var dataItems = ("data-items");
+        var itemClass = ('.item');
+        var id = 0;
+        var btnParentSb = '';
+        var itemsSplit = '';
+        var sampwidth = $(itemsMainDiv).width();
+        var bodyWidth = $('body').width();
+        $(itemsDiv).each(function () {
+            id = id + 1;
+            var itemNumbers = $(this).find(itemClass).length;
+            btnParentSb = $(this).parent().attr(dataItems);
+            itemsSplit = btnParentSb.split(',');
+            $(this).parent().attr("id", "MultiCarousel" + id);
+
+
+            if (bodyWidth >= 1200) {
+                incno = itemsSplit[3];
+                itemWidth = sampwidth / incno;
+            }
+            else if (bodyWidth >= 992) {
+                incno = itemsSplit[2];
+                itemWidth = sampwidth / incno;
+            }
+            else if (bodyWidth >= 768) {
+                incno = itemsSplit[1];
+                itemWidth = sampwidth / incno;
+            }
+            else {
+                incno = itemsSplit[0];
+                itemWidth = sampwidth / incno;
+            }
+            $(this).css({ 'transform': 'translateX(0px)', 'width': itemWidth * itemNumbers });
+            $(this).find(itemClass).each(function () {
+                $(this).outerWidth(itemWidth);
+            });
+
+            $(".leftLst").addClass("over");
+            $(".rightLst").removeClass("over");
+
+        });
+    }
+
+
+    //this function used to move the items
+    function ResCarousel(e, el, s) {
+        var leftBtn = ('.leftLst');
+        var rightBtn = ('.rightLst');
+        var translateXval = '';
+        var divStyle = $(el + ' ' + itemsDiv).css('transform');
+        var values = divStyle.match(/-?[\d\.]+/g);
+        var xds = Math.abs(values[4]);
+        if (e == 0) {
+            translateXval = parseInt(xds) - parseInt(itemWidth * s);
+            $(el + ' ' + rightBtn).removeClass("over");
+
+            if (translateXval <= itemWidth / 2) {
+                translateXval = 0;
+                $(el + ' ' + leftBtn).addClass("over");
+            }
+        }
+        else if (e == 1) {
+            var itemsCondition = $(el).find(itemsDiv).width() - $(el).width();
+            translateXval = parseInt(xds) + parseInt(itemWidth * s);
+            $(el + ' ' + leftBtn).removeClass("over");
+
+            if (translateXval >= itemsCondition - itemWidth / 2) {
+                translateXval = itemsCondition;
+                $(el + ' ' + rightBtn).addClass("over");
+            }
+        }
+        $(el + ' ' + itemsDiv).css('transform', 'translateX(' + -translateXval + 'px)');
+    }
+
+    //It is used to get some elements from btn
+    function click(ell, ee) {
+        var Parent = "#" + $(ee).parent().attr("id");
+        var slide = $(Parent).attr("data-slide");
+        ResCarousel(ell, Parent, slide);
+    }
+
+});
+ 
+})(jQuery); 
